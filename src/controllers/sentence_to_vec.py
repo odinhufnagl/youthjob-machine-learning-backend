7c70c9d192b6795dd76b46d3b88157b2ce5e0659
odinhufnagl <odin.hufnagl@gmail.com>
2023-04-13 14:05:04 +0200

    fix endpoint

--- src/controllers/sentence_to_vec.py
+++ src/controllers/sentence_to_vec.py
@@ -1,9 +1,12 @@
+import json
 from flask import jsonify
 from machine_learning.sentence_to_vec import SwedishSentenceToVecModel
 from services.openai import sentence_to_vec as openai_sentence_to_vec
 import services
 
 
-def sentence_to_vec(sentence: str):
+def sentence_to_vec(request):
+    print(request.json)
+    sentence = request.json['sentence']
     vec = services.texts_to_embeddings([sentence])
     return jsonify(vec[0].tolist())
